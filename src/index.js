553c2930479e3d7d674583834a3a643148769f70
Kodo Yousif <kodo.00257248@gmail.com>
2021-02-27 06:09:12 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
-import LightSwitch from './components/LightSwitch';
 
 ReactDOM.render(
   <LightSwitch />, 
