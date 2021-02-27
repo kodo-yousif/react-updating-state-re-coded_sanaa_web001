cbed50ee6cdee83b7e97c152df71b273870bfbf3
Kodo Yousif <kodo.00257248@gmail.com>
2021-02-27 06:05:44 +0000

    Automatically backed up by Learn

--- src/components/ClickityClick.js
+++ src/components/ClickityClick.js
@@ -1,1 +1,28 @@
-// Code ClickityClick Component Here
+import React from 'react';
+
+class ClickityClick extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+          <h1>{this.state.count}</h1>
+        <button onClick={() => {
+    this.setState(previousState => {
+      return {
+        count: previousState.count + 1
+      }
+    })
+  }}>Click Me</button>
+      </div>
+    );
+  }
+}
+
+export default ClickityClick;
