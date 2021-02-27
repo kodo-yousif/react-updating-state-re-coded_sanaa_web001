4a19a407723c0da8308885efc860bdeadff93d31
Kodo Yousif <kodo.00257248@gmail.com>
2021-02-27 06:09:50 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClickityClick from './components/ClickityClick';
-import ClickityClick from './components/LightSwitch';
+import LightSwitch from './components/LightSwitch';
 ReactDOM.render(
   <LightSwitch />, 
   document.getElementById('root')
